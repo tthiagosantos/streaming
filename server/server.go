1001e7ed1a6c1984c78ecd46a05adc49fa849c31
THIAGO PEREIRA DOS SANTOS <tthiagopereira7@gmail.com>
2024-03-07 23:14:39 -0400

    first commit

---
+++ server/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+)
+
+type StreamServer struct{}
+
+func (ss *StreamServer) ConnectAndReadFile() {
+	ln, err := net.Listen("tcp", ":7777")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go ss.Process(conn)
+	}
+}
+
+func (ss *StreamServer) Process(conn net.Conn) {
+	buf := new(bytes.Buffer)
+	for {
+		var size int64
+		binary.Read(conn, binary.LittleEndian, &size)
+		fmt.Println(size)
+		qtdBytes, err := io.CopyN(buf, conn, size)
+		if err == io.EOF {
+			fmt.Println("EOF")
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(buf.Bytes())
+		fmt.Println("Received", qtdBytes, "bytes from client")
+		break
+	}
+}
+
+func main() {
+	ss := &StreamServer{}
+	ss.ConnectAndReadFile()
+}
